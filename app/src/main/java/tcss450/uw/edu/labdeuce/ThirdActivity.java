51124f6b4495d604bde80309924ca44a69cdcfa6
ungdynasty <ungdynasty@gmail.com>
2016-04-07 10:39:21 -0700

    added 3rd activity!!!!!

--- app/src/main/java/tcss450/uw/edu/labdeuce/ThirdActivity.java
+++ app/src/main/java/tcss450/uw/edu/labdeuce/ThirdActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class secondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +25,5 @@ public class secondActivity extends AppCompatActivity {
             }
         });
     }
-
 
 }
